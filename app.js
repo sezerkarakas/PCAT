fc3d80feae1a082a47fd0c500dc25b6d1eeea9af
sezerking <sezerking123@gmail.com>
2023-03-30 15:36:33 +0300

    Static files uploaded

--- app.js
+++ app.js
@@ -1,3 +1,23 @@
-const fn = function() {
-                                console.log("function working")
-}
+const express = require('express');
+const path = require("path")
+
+const app = express();
+
+
+//MIDDLEWARES
+app.use(express.static('public'));
+
+
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve(__dirname, "temp/index.html"))
+});
+
+app.get('/help', (req, res) => {
+  res.send();
+});
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+  console.log(`Sunucu ${PORT} portunda başlatıldı.`);
+});
